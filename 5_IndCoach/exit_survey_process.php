b1ca7c80c74ece48eb5c4d77992369e1eb898509
tello911 <65243549+tello911@users.noreply.github.com>
2021-05-08 03:07:02 +0800

    sync uni coach and ind coach

---
+++ 5_IndCoach/exit_survey_process.php
@@ -0,0 +1,16 @@
+<?php
+    include '../conn.php';
+
+    if(isset($_POST['name'])){
+        $name = $_POST['name'];
+        $contact = $_POST['contact'];
+        $industry = $_POST['industry'];
+        $comment = $_POST['comment'];
+    }
+
+    $query = "INSERT INTO `survey` (`id`, `name`, `contact`, `industry`, `comment`) VALUES (NULL, '$name', '$contact', '$industry', '$comment');";
+    $results = mysqli_query($conn, $query);
+
+    echo "<script>alert('Your info will being updated');window.location.href='exit_survey.html';</script>";
+
+?>
