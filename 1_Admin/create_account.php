9c61b54acb899ffe1f958393c8f2c9c6614949ee
tello911 <65243549+tello911@users.noreply.github.com>
2021-05-11 01:32:57 +0800

    initialize head and navbar admin

---
+++ 1_Admin/create_account.php
@@ -0,0 +1,137 @@
+﻿<!DOCTYPE html>
+<html>
+
+<?php include 'head.php' ?>
+
+<body class="theme-red">
+   <?php include 'navbar.php' ?>
+
+    <!-- Create Account-->
+    <section class="content">
+        <div class="container-fluid">
+            <div class="block-header">
+                <div class="row clearfix">
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <div class="card">
+                            <div class="header">
+                                <h2>Create Account</h2>
+                            </div>
+                            <form method="POST" action="../create_account_process.php">
+                                <div class="body">
+                                    <div class="row clearfix">
+                                        <div class="col-md-4">
+                                            <p>
+                                                <b>Username</b>
+                                            </p>
+                                            <div class="input-group input-group-lg">
+                                                <span class="input-group-addon">
+                                                    <i class="material-icons">person</i>
+                                                </span>
+                                                <div class="form-line">
+                                                    <input type="text" name="name" class="form-control" placeholder="Username" required>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <p>
+                                                <b>Password</b>
+                                            </p>
+                                            <div class="input-group">
+                                                <div class="form-line">
+                                                    <input type="password" name="password" class="form-control" placeholder="password" required>
+                                                </div>
+                                                <span class="input-group-addon">
+                                                    <i class="material-icons">lock</i>
+                                                </span>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <b>Email Address</b>
+                                            <div class="input-group">
+                                                <span class="input-group-addon">
+                                                    <i class="material-icons">email</i>
+                                                </span>
+                                                <div class="form-line">
+                                                    <input type="text" name="email" class="form-control email" placeholder="Ex: example@example.com" required>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="row clearfix col-md-12 col-lg-12">
+                                        <div class="col-md-4">
+                                            <b>Mobile Phone Number</b>
+                                            <div class="input-group">
+                                                <span class="input-group-addon">
+                                                        <i class="material-icons">phone_iphone</i>
+                                                    </span>
+                                                <div class="form-line">
+                                                    <input type="text" name="phone" class="form-control mobile-phone-number" placeholder="Ex: +00 (000) 000-00-00" required>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <p>ROLES</p>
+                                            <select name="role" class="form-control show-tick">
+                                                <option value="admin">Admin</option>
+                                                <option value="li_comm">2u2i LI Committee</option>
+                                                <option value="student">Student</option>
+                                                <option value="ic">Industry Coach</option>
+                                                <option value="uc">Uni Coach</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <input type="submit" onclick="confirm('Are you sure?');" name="submit" value="Submit" class="btn btn-primary waves-effect">
+                                </div>
+                            </form>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Jquery Core Js -->
+    <script src="../../plugins/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core Js -->
+    <script src="../../plugins/bootstrap/js/bootstrap.js"></script>
+
+    <!-- Select Plugin Js -->
+    <script src="../../plugins/bootstrap-select/js/bootstrap-select.js"></script>
+
+    <!-- Slimscroll Plugin Js -->
+    <script src="../../plugins/jquery-slimscroll/jquery.slimscroll.js"></script>
+
+    <!-- Bootstrap Colorpicker Js -->
+    <script src="../../plugins/bootstrap-colorpicker/js/bootstrap-colorpicker.js"></script>
+
+    <!-- Dropzone Plugin Js -->
+    <script src="../../plugins/dropzone/dropzone.js"></script>
+
+    <!-- Input Mask Plugin Js -->
+    <script src="../../plugins/jquery-inputmask/jquery.inputmask.bundle.js"></script>
+
+    <!-- Multi Select Plugin Js -->
+    <script src="../../plugins/multi-select/js/jquery.multi-select.js"></script>
+
+    <!-- Jquery Spinner Plugin Js -->
+    <script src="../../plugins/jquery-spinner/js/jquery.spinner.js"></script>
+
+    <!-- Bootstrap Tags Input Plugin Js -->
+    <script src="../../plugins/bootstrap-tagsinput/bootstrap-tagsinput.js"></script>
+
+    <!-- noUISlider Plugin Js -->
+    <script src="../../plugins/nouislider/nouislider.js"></script>
+
+    <!-- Waves Effect Plugin Js -->
+    <script src="../../plugins/node-waves/waves.js"></script>
+
+    <!-- Custom Js -->
+    <script src="../../js/admin.js"></script>
+    <script src="../../js/pages/forms/advanced-form-elements.js"></script>
+
+    <!-- Demo Js -->
+    <script src="../../js/demo.js"></script>
+</body>
+
+</html>
---
+++ 1_Admin/upload_li.php
@@ -0,0 +1,88 @@
+﻿<!DOCTYPE html>
+<html>
+
+<?php include 'head.php' ?>
+
+<body class="theme-red">
+   <?php include 'navbar.php' ?>
+
+    <section class="content">
+        <div class="container-fluid">
+            <div class="block-header">
+
+            </div>
+
+            <!-- File Upload | Drag & Drop OR With Click & Choose -->
+            <div class="row clearfix">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="card">
+                        <div class="header">
+                            UPLOAD FILES
+                        </div>
+                        <div class="body">
+                            <form action="../upload_li_process.php" id="frmFileUpload" method="post" enctype='multipart/form-data'>
+                                <div class="dz-message">
+                                    <label>LI PROCESS: DESCRIPTION (will be set as the file name)</label><br><br>
+                                    <input type="text" name="description">
+                                </div>
+                                <div class="fallback">
+                                    <br><br>
+                                    <input type="file" name="file" multiple />
+                                </div>
+                                <div>
+                                    <br><br>
+                                    <input type="submit" onclick="confirm('Are you sure?');" name="submit" value="Submit" class="btn btn-primary waves-effect">
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- #END# File Upload | Drag & Drop OR With Click & Choose -->
+        </div>
+    </section>
+    <!-- Jquery Core Js -->
+    <script src="../../plugins/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core Js -->
+    <script src="../../plugins/bootstrap/js/bootstrap.js"></script>
+
+    <!-- Select Plugin Js -->
+    <script src="../../plugins/bootstrap-select/js/bootstrap-select.js"></script>
+
+    <!-- Slimscroll Plugin Js -->
+    <script src="../../plugins/jquery-slimscroll/jquery.slimscroll.js"></script>
+
+    <!-- Bootstrap Colorpicker Js -->
+    <script src="../../plugins/bootstrap-colorpicker/js/bootstrap-colorpicker.js"></script>
+
+    <!-- Dropzone Plugin Js -->
+    <script src="../../plugins/dropzone/dropzone.js"></script>
+
+    <!-- Input Mask Plugin Js -->
+    <script src="../../plugins/jquery-inputmask/jquery.inputmask.bundle.js"></script>
+
+    <!-- Multi Select Plugin Js -->
+    <script src="../../plugins/multi-select/js/jquery.multi-select.js"></script>
+
+    <!-- Jquery Spinner Plugin Js -->
+    <script src="../../plugins/jquery-spinner/js/jquery.spinner.js"></script>
+
+    <!-- Bootstrap Tags Input Plugin Js -->
+    <script src="../../plugins/bootstrap-tagsinput/bootstrap-tagsinput.js"></script>
+
+    <!-- noUISlider Plugin Js -->
+    <script src="../../plugins/nouislider/nouislider.js"></script>
+
+    <!-- Waves Effect Plugin Js -->
+    <script src="../../plugins/node-waves/waves.js"></script>
+
+    <!-- Custom Js -->
+    <script src="../../js/admin.js"></script>
+    <script src="../../js/pages/forms/advanced-form-elements.js"></script>
+
+    <!-- Demo Js -->
+    <script src="../../js/demo.js"></script>
+</body>
+
+</html>
