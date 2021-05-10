193e6f0aeb212b2f6bfee9efcc9f90f1df3272fe
tello911 <65243549+tello911@users.noreply.github.com>
2021-05-11 02:09:28 +0800

    initialize uni coach head and navbar (2)

---
+++ 4_UniCoach/exit_survey.php
@@ -0,0 +1,189 @@
+﻿<!DOCTYPE html>
+<html>
+
+<?php include 'head.php' ?>
+
+<body class="theme-red">
+    <?php include 'navbar.php' ?>
+
+    <section class="content">
+        <div class="container-fluid">
+            <div class="block-header">
+            </div>
+            <!-- Basic Example | Vertical Layout -->
+            <div class="row clearfix">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="card">
+                        <div class="header">
+                            <h2>Exit Survey</h2>
+                        </div>
+                        <div class="body">
+
+
+                            <body>
+                                <form id="exit_survey" method="POST" action="../exit_survey_process.php" enctype="multipart/form-data">
+                                    <h1>Exit Survey:</h1>
+                                    <!-- One "tab" for each step in the form: -->
+                                    <div class="tab">Name:
+                                        <div class="form-group">
+                                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="name" required></textarea>
+                                        </div>
+                                    </div>
+                                    <div class="tab">Contact Info:
+                                        <div class="form-group">
+                                            <textarea class="form-control" id="exampleFormControlTextarea2" rows="3" name="contact"></textarea>
+                                        </div>
+                                    </div>
+                                    <div class="tab">Industry:
+                                        <div class="form-group">
+                                            <textarea class="form-control" id="exampleFormControlTextarea3" rows="3" name="industry"></textarea>
+                                        </div>
+                                    </div>
+                                    <div class="tab">Comment:
+                                        <div class="form-group">
+                                            <textarea class="form-control" id="exampleFormControlTextarea4" rows="3" name="comment"></textarea>
+                                        </div>
+                                    </div>
+                                    <div style="overflow:auto;">
+                                        <div style="float:right;">
+                                            <button type="button" id="prevBtn" onclick="nextPrev(-1)">Previous</button>
+                                            <button type="button" id="nextBtn" onclick="nextPrev(1)">Next</button>
+                                        </div>
+                                    </div>
+                                    <!-- Circles which indicates the steps of the form: -->
+                                    <div style="text-align:center;margin-top:40px;">
+                                        <span class="step"></span>
+                                        <span class="step"></span>
+                                        <span class="step"></span>
+                                        <span class="step"></span>
+                                    </div>
+                                </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- #END# Basic Example | Vertical Layout -->
+            <div class="row clearfix">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="card">
+                        <div class="header">
+                            <h2>Exit Survey</h2>
+                        </div>
+                        <div class="body">
+                            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSf2UWtUSMp6qcFjyDGgzQh6hv0uUilGxCm53hHSOgtZKHX4zg/viewform?embedded=true" width="640" height="3070" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+    <!-- Jquery Core Js -->
+    <script src="../../plugins/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core Js -->
+    <script src="../../plugins/bootstrap/js/bootstrap.js"></script>
+
+    <!-- Select Plugin Js -->
+    <script src="../../plugins/bootstrap-select/js/bootstrap-select.js"></script>
+
+    <!-- Slimscroll Plugin Js -->
+    <script src="../../plugins/jquery-slimscroll/jquery.slimscroll.js"></script>
+
+    <!-- Jquery Validation Plugin Css -->
+    <script src="../../plugins/jquery-validation/jquery.validate.js"></script>
+
+    <!-- JQuery Steps Plugin Js -->
+    <script src="../../plugins/jquery-steps/jquery.steps.js"></script>
+
+    <!-- Sweet Alert Plugin Js -->
+    <script src="../../plugins/sweetalert/sweetalert.min.js"></script>
+
+    <!-- Waves Effect Plugin Js -->
+    <script src="../../plugins/node-waves/waves.js"></script>
+
+    <!-- Custom Js -->
+    <script src="../../js/admin.js"></script>
+    <script src="../../js/pages/forms/form-wizard.js"></script>
+
+    <!-- Demo Js -->
+    <script src="../../js/demo.js"></script>
+    <script>
+        var currentTab = 0; // Current tab is set to be the first tab (0)
+        showTab(currentTab); // Display the current tab
+
+        function showTab(n) {
+            // This function will display the specified tab of the form...
+            var x = document.getElementsByClassName("tab");
+            x[n].style.display = "block";
+            //... and fix the Previous/Next buttons:
+            if (n == 0) {
+                document.getElementById("prevBtn").style.display = "none";
+            } else {
+                document.getElementById("prevBtn").style.display = "inline";
+            }
+            if (n == (x.length - 1)) {
+                document.getElementById("nextBtn").innerHTML = "Submit";
+            } else {
+                document.getElementById("nextBtn").innerHTML = "Next";
+            }
+            //... and run a function that will display the correct step indicator:
+            fixStepIndicator(n)
+        }
+
+        function nextPrev(n) {
+            // This function will figure out which tab to display
+            var x = document.getElementsByClassName("tab");
+            // Exit the function if any field in the current tab is invalid:
+            if (n == 1 && !validateForm()) return false;
+            // Hide the current tab:
+            x[currentTab].style.display = "none";
+            // Increase or decrease the current tab by 1:
+            currentTab = currentTab + n;
+            // if you have reached the end of the form...
+            if (currentTab >= x.length) {
+                // ... the form gets submitted:
+                document.getElementById("exit_survey").submit();
+                return false;
+            }
+            // Otherwise, display the correct tab:
+            showTab(currentTab);
+        }
+
+        function validateForm() {
+            // This function deals with validation of the form fields
+            var x, y, i, valid = true;
+            x = document.getElementsByClassName("tab");
+            y = x[currentTab].getElementsByTagName("input");
+            // A loop that checks every input field in the current tab:
+            for (i = 0; i < y.length; i++) {
+                // If a field is empty...
+                if (y[i].value == "") {
+                    // add an "invalid" class to the field:
+                    y[i].className += " invalid";
+                    // and set the current valid status to false
+                    valid = false;
+                }
+            }
+            // If the valid status is true, mark the step as finished and valid:
+            if (valid) {
+                document.getElementsByClassName("step")[currentTab].className += " finish";
+            }
+            return valid; // return the valid status
+        }
+
+        function fixStepIndicator(n) {
+            // This function removes the "active" class of all steps...
+            var i, x = document.getElementsByClassName("step");
+            for (i = 0; i < x.length; i++) {
+                x[i].className = x[i].className.replace(" active", "");
+            }
+            //... and adds the "active" class on the current step:
+            x[n].className += " active";
+        }
+    </script>
+    </body>
+
+</html>
